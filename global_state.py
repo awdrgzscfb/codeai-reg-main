4eb1daaa15ceca23fa66c306081832c97d5fdaf6
热友 <763053597@qq.com>
2026-04-20 17:18:08 +0800

    init: upload current code

---
+++ global_state.py
@@ -0,0 +1,28 @@
+import threading
+from collections import deque
+from fastapi import Header, HTTPException
+from utils import core_engine
+import utils.config as cfg
+
+VALID_TOKENS = set()
+CLUSTER_NODES = {}
+NODE_COMMANDS = {}
+cluster_lock = threading.Lock()
+log_history = []
+worker_status: dict = {}
+engine = core_engine.RegEngine()
+
+
+def append_log(msg: str):
+    log_history.append(msg)
+    limit = getattr(cfg, 'MAX_LOG_LINES', 500)
+    if len(log_history) > limit * 1.2:
+        del log_history[:-limit]
+
+async def verify_token(authorization: str = Header(None)):
+    if not authorization or not authorization.startswith("Bearer "):
+        raise HTTPException(status_code=401, detail="未提供有效凭证")
+    token = authorization.split(" ")[1]
+    if token not in VALID_TOKENS:
+        raise HTTPException(status_code=401, detail="登录已过期，请重新登录")
+    return token
